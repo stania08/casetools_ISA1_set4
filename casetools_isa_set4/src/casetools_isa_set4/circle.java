721d0a72b0fe66d7c624af0ee2c8e4d6b932b91c
stania08 <staniarebelo08@gmail.com>
2024-02-08 10:15:08 +0530

    fbggd

---
+++ casetools_isa_set4/src/casetools_isa_set4/circle.java
@@ -0,0 +1,15 @@
+package casetools_isa_set4;
+
+public class circle {
+	private double radius;
+	public circle(double radius) {
+		this.radius=radius;
+	}
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double radius) {
+		this.radius=radius;
+	}
+
+}
